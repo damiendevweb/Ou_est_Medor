945db2896807a9aab5b634bf135563b7b73b9aa3
Damien <yorlin@hotmail.fr>
2026-03-29 22:56:20 +0200

    Update config pwa

--- vite.config.ts
+++ vite.config.ts
@@ -12,71 +12,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: { enabled: true },
       includeAssets: ['favicon.ico', 'icons/*.png', 'robots.txt'],
-      manifest: {
-        name: 'DoggyTracker',
-        short_name: 'DoggyTracker',
-        description: 'Suivi de localisation et gestion de vos chiens',
-        start_url: '/',
-        scope: '/',
-        display: 'standalone',
-        orientation: 'portrait-primary',
-        theme_color: '#01696f',
-        background_color: '#f7f6f2',
-        lang: 'fr',
-        icons: [
-          {
-            "src": "icons/icon-48x48.png",
-            "sizes": "48x48",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/icon-72x72.png",
-            "sizes": "72x72",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/icon-96x96.png",
-            "sizes": "96x96",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/icon-128x128.png",
-            "sizes": "128x128",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/icon-144x144.png",
-            "sizes": "144x144",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/icon-152x152.png",
-            "sizes": "152x152",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/icon-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/icon-256x256.png",
-            "sizes": "256x256",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/icon-384x384.png",
-            "sizes": "384x384",
-            "type": "image/png"
-          },
-          {
-            "src": "icons/icon-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png"
-          }
-        ],
-        categories: ['lifestyle', 'utilities']
-      },
+      manifest: false,
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
